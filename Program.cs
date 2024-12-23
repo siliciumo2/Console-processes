8447085663e0af7a0bf799bab4b5c1390aadf189
Siliciumo2 <147613900+siliciumo2@users.noreply.github.com>
2024-12-23 11:20:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+class Program
+{
+    static void Main()
+    {
+        Process[] processes = Process.GetProcesses();
+        Console.WriteLine("PID\tName");
+        foreach (Process process in processes)
+        {
+            Console.WriteLine($"{process.Id}\t{process.ProcessName}");
+        }
+    }
+}
